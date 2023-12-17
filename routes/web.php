aed6f673570688ee177f39b8d641ac707109600c
YounesAT1 <youness.monkyde@gmail.com>
2023-12-17 16:08:05 +0100

    adding tailwind

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CRUD_STUDENT\etudiantController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//? CRUD_STUDENT
+
+Route::resource('CRUD_STUDENT', etudiantController::class);
